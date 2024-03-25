662620f20ffa78c55a93ae8c6b8f0a97e8802eae
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-03-24 23:59:24 -0600

    changes

--- php scripts/soccer_queries.php
+++ php scripts/soccer_queries.php
@@ -1,5 +1,6 @@
 <?php
-$league_table = "SELECT * FROM `teams` ORDER BY (`wins_team` * 3) + (`draws_team` * 1) DESC"; // Suma los puntos de victoria, más los de empates, según eso, se ordena
+$league_table = "SELECT * FROM `teams` ORDER BY (`wins_team` * 3) + (`draws_team` * 1) DESC;"; // Suma los puntos de victoria, más los de empates, según eso, se ordena
 $top_scorers = "SELECT * FROM `players` ORDER BY (`goals_player`) DESC;";
 $top_assisters = "SELECT * FROM `players` ORDER BY (`assists_player`) DESC;";
-$x_team_players = "SELECT * FROM `players` WHERE (`player_team_id` = 1) ORDER BY (`dorsal_player`) ASC";
+$top_fouls = "SELECT * FROM `players` ORDER BY (`fouls_player`) DESC;";
+$x_team_players = "SELECT * FROM `players` WHERE (`player_team_id` = 1) ORDER BY (`dorsal_player`) ASC;";
