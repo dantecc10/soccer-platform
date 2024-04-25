30506446a40d4609a2bc42bc0db52ced12f849f5
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-25 15:04:41 -0600

    Grid work

--- assets/js/soccer.js
+++ assets/js/soccer.js
@@ -9,3 +9,15 @@ function set_zeros() {
 }
 
 set_zeros();
+
+function upload_image(element) {
+    element.closest('td').querySelectorAll("input")[0].click();
+}
+function paint_upload_button(element) {
+    if (this.value != '') {
+        element.closest('td').querySelectorAll("button")[0].classList.add("loaded-img-button");
+    } else {
+        element.closest('td').querySelectorAll("button")[0].classList.remove("loaded-img-button");
+        element.closest('td').querySelectorAll("button")[0].textContent = "✅ Archivo cargado";
+    }
+}
