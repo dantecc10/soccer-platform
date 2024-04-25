9dd2ac053d110a184505227d02184b194127cae1
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-25 01:03:02 -0600

    ..

--- php scripts/actions.php
+++ php scripts/actions.php
@@ -4,8 +4,7 @@ if (isset($_SESSION['id']) || isset($_GET['type'])) {
     include "functions.php";
     switch ($_GET['type']) {
         case 'add-team':
-            
-            $result = add_team($_POST['league_team'], $_POST['team_name'], $_POST['logo_team'], $_POST['couch_team'], $_POST['description_name']);
+            $result = add_team($_POST['league_team'], $_POST['team_name'], $_FILES['logo_team'], $_POST['couch_team'], $_POST['description_name']);
             if ($result != false) {
                 save_team_logo($result, $_FILES['logo_team']);
 
