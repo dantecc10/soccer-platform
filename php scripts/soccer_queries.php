90777af9c450a82c5086a62ac0f3cec0bd698db0
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-26 11:53:39 -0600

    ..

--- php scripts/soccer_queries.php
+++ php scripts/soccer_queries.php
@@ -148,5 +148,5 @@ $league_table_fields = [
 ];
 
 $query = $match_basic_data_queries[0];
-
+$nuevas_condiciones = ("(nada = nada)");
 echo (preg_replace('/WHERE (.+?) (ORDER BY|$)/', 'WHERE '.$nuevas_condiciones.' $2', $query));
