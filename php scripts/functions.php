40f67e812c3169e2ee599264cdacbb20fa14edbf
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-28 02:39:10 -0600

    ""

--- php scripts/functions.php
+++ php scripts/functions.php
@@ -732,8 +732,8 @@ function fetch_player_cards($id)
 
 
     $cards_dom = "";
-    $sql = ('SELECT p`.`id_player`, `p`.`name_player`, `p`.`nickname_player`, `p`.`last_names_player`, `p`.`dorsal_player`, `p`.`img_player`,
-    t.icon_team AS team_icon, `p`.`player_team_id` FROM players p LEFT JOIN teams t ON p.player_team_id = t.id_team WHERE (`player_team_id` = ' . $id . ');');
+    $sql = ("SELECT p`.`id_player`, `p`.`name_player`, `p`.`nickname_player`, `p`.`last_names_player`, `p`.`dorsal_player`, `p`.`img_player`,
+    t.icon_team AS team_icon, `p`.`player_team_id` FROM players p LEFT JOIN teams t ON p.player_team_id = t.id_team WHERE (`player_team_id` = $id);");
 
     $cards = fetch_fields('players', $fields, '', $sql);
     for ($i = 0; $i < sizeof($cards); $i++) {
