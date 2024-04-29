bb8170a613840fb408c6ba5e9d6f44faf46af564
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-29 13:25:43 -0600

    check function

--- php scripts/debugger.php
+++ php scripts/debugger.php
@@ -5,3 +5,9 @@ session_start();
 $_SESSION['logged_in'] = true;
 $_SESSION['id_user'] = 5;
 $_SESSION['name_user'] = "Dante";
+
+if (logged_in()) {
+    echo "You are logged in";
+} else {
+    echo "You are not logged in";
+}
