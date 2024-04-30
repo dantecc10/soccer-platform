6da7a2d5c9089f23b0723b4eae6188e823e7b1a4
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-30 00:53:43 -0600

    ..

--- add-match.php
+++ add-match.php
@@ -99,7 +99,7 @@ if (!isset($_SESSION['id_user']) && !isset($_SESSION['logged_in'])) {
                                 </div>
                                 <div class="row">
                                     <div class="col my-2">
-                                        <div class="input-group"><span class="input-group-text main-bg-color submain-color col-12 col-sm-auto col-md-3 justify-content-center fw-bolder fs-5">Equipo Visitante *</span><select class="form-select form-control main-color submain-bg-color custom-font text-center main-border fs-5" id="visitor-team-1" name="visitor-team" required>
+                                        <div class="input-group"><span class="input-group-text main-bg-color submain-color col-12 col-sm-auto col-md-3 justify-content-center fw-bolder fs-5">Equipo Visitante *</span><select class="form-select form-control main-color submain-bg-color custom-font text-center main-border fs-5" id="visitor-team" name="visitor-team" required>
                                                 <optgroup label="Equipos">
                                                     <option>Selecciona un equipo</option>
                                                     <?php
