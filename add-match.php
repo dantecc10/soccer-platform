e1cb8719a151729863ae6d705b90d9e6f2df1442
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-30 00:28:37 -0600

    fetch teams

--- add-match.php
+++ add-match.php
@@ -64,13 +64,21 @@ if (!isset($_SESSION['id_user']) && !isset($_SESSION['logged_in'])) {
                     <div class="col">
                         <form id="add-match-form" class="custom-font" action="php scripts/actions.php?type=add-match" method="post">
                             <div class="col">
+                                <?php
+                                include "php scripts/soccer_queries.php";
+                                $teams = fetch_fields("teams", ['id_team', 'name_team'], null, null);
+                                ?>
                                 <div class="row fs-5">
                                     <div class="col my-2">
                                         <div class="input-group"><span class="input-group-text main-bg-color submain-color col-12 col-sm-auto col-md-3 justify-content-center fw-bolder fs-5">Selecciona la liga *</span><select class="form-select form-control main-color submain-bg-color custom-font text-center main-border fs-5" id="league-match" name="league-match" required="">
-                                                <optgroup label="This is a group">
-                                                    <option value="12" selected="">This is item 1</option>
-                                                    <option value="13">This is item 2</option>
-                                                    <option value="14">This is item 3</option>
+                                                <optgroup label="Ligas disponibles">
+                                                    <option value="" selected="">Selecciona</option>
+                                                    <!--<option value="1">Liga de Chignahuapan</option>-->
+                                                    <?php
+                                                    for ($i = 0; $i < sizeof($teams); $i++) {
+                                                        echo "<option value='" . $teams[$i][0] . "'>" . $teams[$i][1] . "</option>";
+                                                    }
+                                                    ?>
                                                 </optgroup>
                                             </select></div>
                                     </div>
@@ -83,8 +91,8 @@ if (!isset($_SESSION['id_user']) && !isset($_SESSION['logged_in'])) {
                                 <div class="row">
                                     <div class="col my-2">
                                         <div class="input-group"><span class="input-group-text main-bg-color submain-color col-12 col-sm-auto col-md-3 justify-content-center fw-bolder fs-5">Equipo local *</span><select class="form-select form-control main-color submain-bg-color custom-font text-center main-border fs-5" id="local-team" name="local-team" required="">
-                                                <optgroup label="This is a group">
-                                                    <option value="12" selected="">This is item 1</option>
+                                                <optgroup label="Equipos">
+                                                    <option value="" selected="">Selecciona un equipo</option>
                                                     <option value="13">This is item 2</option>
                                                     <option value="14">This is item 3</option>
                                                 </optgroup>
