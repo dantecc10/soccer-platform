cb01511ee88085f90eb0008229ee79648cb8b361
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-05-16 12:02:07 -0600

    get id

--- assets/js/live-data.js
+++ assets/js/live-data.js
@@ -3,7 +3,8 @@ function update_live_stats() {
     const containers_array = document.querySelectorAll(query);
     for (let index = 0; index < containers_array.length; index++) {
         const element = containers_array[index];
-        console.log("Elemento " + index);
+        const id = element.getAttribute('id');
+        console.log("Elemento " + index + "; id: " + id);
     }
     console.log("Hubo " + containers_array.length + " elementos");
 }
