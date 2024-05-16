22a5642561c03e43c021c3f99401bc5ae8cb6b91
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-05-16 11:59:22 -0600

    ..

--- assets/js/live-data.js
+++ assets/js/live-data.js
@@ -2,8 +2,8 @@ function update_live_stats() {
     const query = (".events-container");
     const containers_array = document.querySelectorAll(query);
     for (let index = 0; index < containers_array.length; index++) {
-        const element = array[index];
+        const element = containers_array[index];
         console.log("Elemento " + index);
     }
-    console.log("Hubo " + array.length + " elementos");
+    console.log("Hubo " + containers_array.length + " elementos");
 }
