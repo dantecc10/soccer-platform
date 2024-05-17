87cd83775ceb49e5aa20e09182544438cb002423
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-05-17 16:14:34 -0600

    Streak

--- index.php
+++ index.php
@@ -98,7 +98,8 @@ session_start();
                                 <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
                                 <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
                                 <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
-                            </svg>&nbsp;<?php echo ((logged_in()) ? $_SESSION['name_user'] : "Entrar"); ?></a></li>
+                            </svg>&nbsp;<?php echo ((logged_in()) ? $_SESSION['name_user'] : "Entrar"); ?></a>
+                    </li>
                 </ul>
             </div>
         </div>
