150954feb6a2160657e7577f4a0283c758b90816
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-12-30 12:56:30 -0600

    fixing details

--- index.php
+++ index.php
@@ -203,8 +203,6 @@ session_start();
                                         <?php
                                         //echo (matches_output(fetch_matches(2, null)));
                                         //Disable warnings
-                                        //error_reporting(E_ERROR | E_PARSE);
-                                        echo (detailed_matches_output(0, null))
                                         ?>
                                         <div class="row text-center rounded-3 my-2" style="border: 1px solid var(--submain-background-color);background: linear-gradient(90deg, var(--main-background-color), rgba(173,238,0,0.54));">
                                             <div class="col"><a class="submain-color" href="matches.html#jugados"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-time-duration-90">
